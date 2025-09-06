e9c9ec767fa24708ecdb41c4df68a821ab4b43be
stevihiene-git <122815958+stevihiene-git@users.noreply.github.com>
2025-09-06 10:35:35 +0100

    Update models.py

--- note_app/models.py
+++ note_app/models.py
@@ -1,5 +1,5 @@
 # models.py (new file)
-from note_app import db, login_manager
+from note_app import db, login_manager, create_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
